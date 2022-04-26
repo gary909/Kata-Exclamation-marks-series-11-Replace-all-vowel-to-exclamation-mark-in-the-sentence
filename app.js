a3f6d33320a4c33a9f4c9d75521423c16f959bcb
gary <gary.white@lodgit.com>
2022-04-26 13:29:09 +0200

    code working

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function replace(s) {
     //coding and coding....
-
-
+    let splitString = s.split("");
+    let newArr = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U') {
+            newArr.push("!");
+        } else {
+            newArr.push(s[i]);
+        }
+    }
+    return newArr.join("");
 }
 
 console.log(replace("Hi!")); // H!!
