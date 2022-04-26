1fe1c30cfa9759a61e1e55da99c523542ff305bc
gary <gary.white@lodgit.com>
2022-04-26 13:04:47 +0200

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function replace(s) {
+    //coding and coding....
+
+
+}
+
+console.log(replace("Hi!")); // H!!
+console.log(replace("!Hi! Hi!")); // !H!! H!!
+console.log(replace("aeiou")); // !!!!!
+console.log(replace("ABCDE")); // !BCD!
